709d982101a7d278c44c338c50bd64c9c9828b65
nocturn-le-chat <zimlin.nazar@yandex.ru>
2026-04-08 15:46:50 +0700

    Sortion ver.0.1

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def workhorse(iterable):
-  return iterable
+    if len(iterable) <= 1: return iterable 
+    slices = [[], iterable[len(iterable)//2], []]
+    for k in iterable:
+        if k < slices[1]: slices[0].append(k)
+        if k > slices[1]: slices[2].append(k)
+    
+    return workhorse(slices[0]) + [slices[1]] + workhorse(slices[2])
 
 def qsort(iterable):
-  return workhorse(iterable)
+    if type(iterable) != list: return "ERROR: unsupported input type"
+    elif not all([type(k) in [int, str] for k in iterable]): return "ERROR: incorrect input containments"
+    else: return workhorse(iterable)
