d9f1938c69d2c908f04d47c3d71e37810b810ba5
nocturn-le-chat <zimlin.nazar@yandex.ru>
2026-04-08 16:38:00 +0700

    Infinite recursion

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 def workhorse(iterable):
-    if len(iterable) <= 1: return iterable 
-    slices = [[], iterable[len(iterable)//2], []]
-    for k in iterable:
-        if k < slices[1]: slices[0].append(k)
-        if k > slices[1]: slices[2].append(k)
+    if len(iterable) <=1: return iterable
+    anchor = iterable[len(iterable)//2]
+    left = [k for k in iterable if k <= anchor]
+    right = [k for k in iterable if k > anchor]
     
-    return workhorse(slices[0]) + [slices[1]] + workhorse(slices[2])
+    return workhorse(left) + workhorse(right)
 
 def qsort(iterable):
     if type(iterable) != list: return "ERROR: unsupported input type"
     elif not all([type(k) in [int, str] for k in iterable]): return "ERROR: incorrect input containments"
     else: return workhorse(iterable)
+
+print(qsort([1, 3, 2, 2, 5, 4, 6, 3]))
